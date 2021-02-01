19368ba2a54b623cf427ed0eeab5441cccffb8e9
Filipe Alvares <alvares.filipeoliveira@gmail.com>
2021-02-01 16:13:20 -0300

    Complete crud

--- v-db/index.js
+++ v-db/index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
-app.get('/api/get', (req,res)=>{
+app.get('/api/get', (req, res) => {
   const sqlSelect =
     "SELECT * FROM movie_reviews";
   db.query(sqlSelect, (err, result) => {
@@ -33,6 +33,29 @@ app.post("/api/insert", (req, res) => {
     "INSERT INTO movie_reviews(movieName, movieReview) VALUES (?,?)";
   db.query(sqlInsert, [movieName, movieReview], (err, result) => {
     console.log(result);
+  });
+});
+
+
+app.delete('/api/delete/:movieName', (req, res) => {
+  const name = req.params.movieName;
+  const sqlDelete =
+    "DELETE FROM movie_reviews where movieName = ?";
+
+  db.query(sqlDelete, name, (err, result) => {
+    if (err) console.log(err)
+  });
+});
+
+
+app.put("/api/update", (req, res) => {
+  const name = req.body.movieName;
+  const review = req.body.movieReview;
+  const sqlUpdate =
+    "UPDATE movie_reviews SET  movieReview = ? where movieName = ?";
+
+  db.query(sqlUpdate, [review, name], (err, result) => {
+    if (err) console.log(err)
   });
 });
 
--- v-web/src/App.js
+++ v-web/src/App.js
@@ -8,6 +8,8 @@ function App() {
   const [review, setReview] = useState("");
   const [movieReviewList, setMovieList] = useState([]);
 
+  const [newReview, setNewReview] = useState("");
+
   useEffect(() => {
     Axios.get("http://localhost:3001/api/get").then((response) => {
       setMovieList(response.data);
@@ -18,10 +20,28 @@ function App() {
     Axios.post("http://localhost:3001/api/insert", {
       movieName: movieName,
       movieReview: review,
-    }).then(() => {
-      alert("successfull insert");
     });
+
+    setMovieList([
+      ...movieReviewList,
+      { movieName: movieName, movieReview: review }
+    ]);
+
   };
+
+  const deleteReview = (movie) => {
+    Axios.delete(`http://localhost:3001/api/delete/${movie}`);
+  }
+
+  const updateReview = (movie) => {
+    Axios.put("http://localhost:3001/api/update", {
+      movieName: movie,
+      movieReview: newReview,
+    });
+
+    setNewReview("");
+  };
+
 
   return (
     <div className="App">
@@ -47,9 +67,29 @@ function App() {
 
         {movieReviewList.map((val) => {
           return (
-            <h2>
-              Moviename: {val.movieName} | Movie Review: {val.movieReview}
-            </h2>
+
+            <div className="card">
+              <h1>{val.movieName}</h1>
+              <p>{val.movieReview}</p>
+
+              <button
+                onClick={() => {
+                  deleteReview(val.movieName)
+                }}>
+                Delete
+              </button>
+
+              <input type="text" id="updateInput" onChange={(e) => {
+                setNewReview(e.target.value);
+              }} />
+
+              <button onClick={() => {
+                updateReview(val.movieName)
+              }}>
+                Update
+              </button>
+
+            </div>
           );
         })}
       </div>
